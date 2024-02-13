a0b86e14d072a43b7ddae7988c77b9c23742d14f
HalfMillennium <glc2266@gmail.com>
2024-02-13 18:24:34 -0500

    Introduce placeholder image on bottom-left of quote card on ShowThoughtsPage.tsx. Eventually, this will contain an image of the person searched for on the previous page. Also add note about implementing Redux to avoid an instance of prop-drilling.

--- src/features/thoughts/QuoteCard.tsx
+++ src/features/thoughts/QuoteCard.tsx
@@ -4,13 +4,18 @@ import '../styles/thoughts/QuoteCard.css';
 
 const QUOTE_ICON_SIZE = 42;
 
-export default function ThoughtDisplay(props: {quote: string}) {
+export default function QuoteCard(props: {quote: string}) {
     const quote = props.quote;
     return (
         <>
             <div className="quote-container">
                 <div className="row qoute-left-container"><FaQuoteLeft className="pad-top-2" size={QUOTE_ICON_SIZE}/></div>
-                <p className="quote">{quote}</p>
+                <p className="quote">
+                    {quote}
+                    <div className="floated-left">
+                        <img className="person-image" src="https://via.placeholder.com/200" alt="placeholder"></img>
+                    </div>
+                </p>
                 <div className="row quote-right-container"><FaQuoteRight className="pad-bot-2" size={QUOTE_ICON_SIZE}/></div>
             </div>
         </>
--- src/features/thoughts/ShowThoughtsPage.tsx
+++ src/features/thoughts/ShowThoughtsPage.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import ThoughtDisplay from './QuoteCard'
+import QuoteCard from './QuoteCard'
 import { EXAMPLE_QUOTE } from '../../testing/example_quote';
 
 export function ShowThoughtsPage(props: {searchTerm: string}) {
-    const searchTerm = props.searchTerm;
+    // const searchTerm = props.searchTerm; -> Use Redux for this instead of prop drilling
     return (
         <div className="container">
             <p className="preamble">And the quote of the day is...</p>
-            <ThoughtDisplay quote={EXAMPLE_QUOTE}/>
+            <QuoteCard quote={EXAMPLE_QUOTE}/>
         </div>
     )
 }
