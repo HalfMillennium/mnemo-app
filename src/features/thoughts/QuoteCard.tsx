031d1e561dee860c152b22770fe670f7f5553b0e
HalfMillennium <glc2266@gmail.com>
2024-02-14 01:48:38 -0500

    Add second placeholder image on the top right of the quote card. It looks quite weird at the moment.

--- src/features/thoughts/QuoteCard.tsx
+++ src/features/thoughts/QuoteCard.tsx
@@ -11,9 +11,12 @@ export default function QuoteCard(props: {quote: string}) {
             <div className="quote-container">
                 <div className="row qoute-left-container"><FaQuoteLeft className="pad-top-2" size={QUOTE_ICON_SIZE}/></div>
                 <p className="quote">
+                    <div className="floated-right">
+                        <img className="person-image-top" src="https://via.placeholder.com/200" alt="placeholder"></img>
+                    </div>
                     {quote}
                     <div className="floated-left">
-                        <img className="person-image" src="https://via.placeholder.com/200" alt="placeholder"></img>
+                        <img className="person-image-bottom" src="https://via.placeholder.com/200" alt="placeholder"></img>
                     </div>
                 </p>
                 <div className="row quote-right-container"><FaQuoteRight className="pad-bot-2" size={QUOTE_ICON_SIZE}/></div>
