5967419c73590b0a6ce049eb859507f1506d3bec
HalfMillennium <glc2266@gmail.com>
2024-02-21 23:34:55 -0500

    Minor style updates.

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import '../styles/Header.css'
 
 export function Header() {
+    const siteTitleDisplay = "mnemo"
     return (
-    <div className="header-container">
-        <div className="header">
-            mnemo
+        <div className="header-container">
+            <div className="header">
+                {siteTitleDisplay}
+            </div>
         </div>
-    </div>
     );
 }
