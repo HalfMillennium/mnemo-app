3ee8ded6098a1da09351e3351f3f5edd4c181848
HalfMillennium <glc2266@gmail.com>
2024-02-27 19:00:47 -0500

    Continue attempt to enhance the footer component

--- src/core/components/Footer.tsx
+++ src/core/components/Footer.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import '../styles/Footer.css';
 
 export function Footer() {
-    return <>This is the footer...</>;
+    return (
+        <div className="footer-container">
+            This is a footer containing text
+        </div>
+    );
 }
