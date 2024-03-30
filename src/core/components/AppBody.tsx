a8f3d8ca4cdb1a128ce58dece935bea853df1012
HalfMillennium <glc2266@gmail.com>
2024-03-30 16:37:02 -0400

    Update title and favicon. Title is set according to current 'tab', which will be either 'Search Entries' or 'View Diary'

--- src/core/components/AppBody.tsx
+++ src/core/components/AppBody.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import {DiaryEntryPage} from '../../features/diary_entry';
 import { SearchDiariesPage } from "../../features/diary_entry";
 import '../styles/AppBody.css';
+import { useEffect } from "react";
 
 export function AppBody() {
+  let currentTabTitle = 'Search Entries'
+  useEffect(() => {
+    document.title = `MNEMO | ${currentTabTitle}`
+  }, []);
   return (
       <div className="app-body">
         <div className="app-body-main">
