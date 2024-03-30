e50b375bc162f9d6d2658df8c7235d0003b4ad9f
HalfMillennium <glc2266@gmail.com>
2024-03-30 13:53:56 -0400

    Introduce IconButton for back button styling. Add media query for QuoteCard component to adjust to smaller screens.

--- src/shared/card-display/BioCardDisplay.tsx
+++ src/shared/card-display/BioCardDisplay.tsx
@@ -4,10 +4,12 @@ import './BioCardDisplay.css';
 export function BioCardDisplay(props: {entityName: string}) {
     return (
         <div className="bio-card-parent">
-            <p className="bio-text">
+            <div>
                 <img src="https://placehold.co/100x100" className="bio-image"/>
+            </div>
+            <span className="bio-text">
                 {props.entityName} is an American actor known for his versatile and intense performances on stage and screen, with notable roles in "Dangerous Liaisons" and "Being John Malkovich."
-            </p>
+            </span>
         </div>
     );
 }
