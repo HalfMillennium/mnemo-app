9983eb49cde768caac5005d55f7ea6c4869d58c1
HalfMillennium <glc2266@gmail.com>
2024-04-21 18:11:46 -0400

    Refactor AppBody.css into AppBody.module.css. Update usage accordingly. Also hide scrollbar app-wide. This vertical should never be useful anyway due to character count maximums on model output generation.

--- src/core/components/AppBody.tsx
+++ src/core/components/AppBody.tsx
@@ -1,28 +1,27 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom';
-import {DiaryEntryPage} from '../../features/diary_entry_page';
+import { Route, Routes } from "react-router-dom";
+import { DiaryEntryPage } from "../../features/diary_entry_page";
 import { SearchDiariesPage } from "../../features/search_diaries_page";
-import '../styles/AppBody.css';
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
+import styles from "../styles/AppBody.module.css";
 
 export function AppBody() {
-  const [entityName, setEntityName] = useState('');
-  const currentEntityName = useSelector((state: RootState) => state.namePrompt.name);
-
-  useEffect(() => {
-    setEntityName(currentEntityName ?? '');
-  }, [currentEntityName]);
+  const currentEntityName = useSelector(
+    (state: RootState) => state.namePrompt.name
+  );
 
   return (
-      <div className="app-body">
-        <div className="app-body-main">
-          <Routes>
-            <Route path="/" element={<SearchDiariesPage/>} />
-            <Route path="/journal" element={<DiaryEntryPage entityName={entityName}/>} />
-          </Routes>
-        </div>
+    <div className={styles["app-body"]}>
+      <div className={styles["app-body-main"]}>
+        <Routes>
+          <Route path="/" element={<SearchDiariesPage />} />
+          <Route
+            path="/journal"
+            element={<DiaryEntryPage entityName={currentEntityName} />}
+          />
+        </Routes>
       </div>
+    </div>
   );
 }
