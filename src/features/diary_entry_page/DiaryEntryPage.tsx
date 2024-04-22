5957872e67c817d79e6aa36add39ed87fcf70a74
HalfMillennium <glc2266@gmail.com>
2024-04-21 22:57:40 -0400

    Update ProgressPage styling to ensure text stands out from background.

--- src/features/diary_entry_page/DiaryEntryPage.tsx
+++ src/features/diary_entry_page/DiaryEntryPage.tsx
@@ -1,48 +1,50 @@
-import React from 'react'
-import QuoteCard from '../../shared/quote-card/QuoteCard'
-import { EXAMPLE_QUOTE } from '../../testing/example_quote';
-import styles from './DiaryEntryPage.module.css';
-import { BackButton } from '../../shared/back-button/BackButton';
-import { DiaryEntryTitle } from '../../shared/diary-entry-title/DiaryEntryTitle';
-import { ProgressPage } from '../../shared/progress-page/ProgressPage';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../core/store/store';
+import React from "react";
+import QuoteCard from "../../shared/quote-card/QuoteCard";
+import { EXAMPLE_QUOTE } from "../../testing/example_quote";
+import styles from "./DiaryEntryPage.module.css";
+import { BackButton } from "../../shared/back-button/BackButton";
+import { DiaryEntryTitle } from "../../shared/diary-entry-title/DiaryEntryTitle";
+import { ProgressPage } from "../../shared/progress-page/ProgressPage";
+import { useSelector } from "react-redux";
+import { RootState } from "../../core/store/store";
 
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
-export function DiaryEntryPage(props: {entityName: string|undefined}) {
-    const {entityName} = props;
-    const backLink = '/'; // TODO: Replace with actual back link
-    const loadingStatus = useSelector((state: RootState) => state.journalEntries.loading);
+export function DiaryEntryPage(props: { entityName: string | undefined }) {
+  const { entityName } = props;
+  const backLink = "/"; // TODO: Replace with actual back link
+  const loadingStatus = useSelector(
+    (state: RootState) => state.journalEntries.loading
+  );
 
-    if(!loadingStatus) {
-        return (
-            <>
-                <Helmet>
-                    <title>| The Journal of {entityName}</title>
-                </Helmet>
-                <div className={styles['container']}>
-                    <div className={styles['page-header']}>
-                        <div className={styles['back-button-container']}>
-                            <BackButton buttonText='BACK TO SEARCH' destination={backLink}/>
-                        </div>
-                        <div className={styles['diary-entry-title']}>
-                            <DiaryEntryTitle entityName={entityName}/>
-                        </div>
-                    </div>
-                    <div className={styles['quote-card-container']}>
-                        <QuoteCard quote={EXAMPLE_QUOTE} entityName={entityName}/>
-                    </div>
-                </div>
-            </>
-        );
-    }
+  if (!loadingStatus) {
     return (
-        <>
-            <Helmet>
-                <title>| Making magic happen...</title>
-            </Helmet>
-            <ProgressPage entityName={entityName}/>
-        </>
+      <>
+        <Helmet>
+          <title>| The Journal of {entityName}</title>
+        </Helmet>
+        <div className={styles["container"]}>
+          <div className={styles["page-header"]}>
+            <div className={styles["back-button-container"]}>
+              <BackButton buttonText="BACK TO SEARCH" destination={backLink} />
+            </div>
+            <div className={styles["diary-entry-title"]}>
+              <DiaryEntryTitle entityName={entityName} />
+            </div>
+          </div>
+          <div className={styles["quote-card-container"]}>
+            <QuoteCard quote={EXAMPLE_QUOTE} entityName={entityName} />
+          </div>
+        </div>
+      </>
     );
+  }
+  return (
+    <>
+      <Helmet>
+        <title>| Making magic happen...</title>
+      </Helmet>
+      <ProgressPage entityName={entityName} />
+    </>
+  );
 }
--- src/shared/progress-page/ProgressPage.tsx
+++ src/shared/progress-page/ProgressPage.tsx
@@ -1,19 +1,23 @@
 import React from "react";
-import styles from './ProgressPage.module.css';
-import {Button, CircularProgress} from '@mui/material';
+import styles from "./ProgressPage.module.css";
+import { Button, CircularProgress } from "@mui/material";
 
-export function ProgressPage(props: {entityName: string|undefined}) {
-    return (
-        <div className={styles['progress-container']}>
-            <div className={styles['progress-bar']}>
-                <CircularProgress color="inherit" size={128}/>
-            </div>
-            <div className={styles['progress-bar-text']}>
-                <p>Loading the intimate thoughts of {props.entityName}...</p>
-                <div>
-                    <Button variant="outlined" color="inherit">¿Cancel?</Button>
-                </div>
-            </div>
+export function ProgressPage(props: { entityName: string | undefined }) {
+  return (
+    <div className={styles["progress-container"]}>
+      <div className={styles["progress-bar"]}>
+        <CircularProgress color="inherit" size={128} />
+      </div>
+      <div className={styles["progress-bar-actions"]}>
+        <p className={styles["progress-bar-text"]}>
+          Loading the intimate thoughts of {props.entityName}...
+        </p>
+        <div>
+          <Button variant="outlined" color="inherit">
+            ¿Cancel?
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
