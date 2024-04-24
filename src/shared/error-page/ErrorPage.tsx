0e6e4284562d722f18869314f4873342e39bb618
HalfMillennium <glc2266@gmail.com>
2024-04-24 01:12:08 -0400

    Add error icon to ErrorPage

--- src/shared/error-page/ErrorPage.tsx
+++ src/shared/error-page/ErrorPage.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { clearError } from "../../core/store/journal_entries/journalEntriesSlice";
+import { MdErrorOutline } from "react-icons/md";
 
 export function ErrorPage(props: { text: string }) {
   const location = useLocation();
@@ -15,6 +16,9 @@ export function ErrorPage(props: { text: string }) {
   return (
     <div className={styles["error-body"]}>
       <div className={styles["error-card"]}>
+        <div className={styles["error-icon"]}>
+          <MdErrorOutline color="red" size="96" />
+        </div>
         <h1>Uh oh!</h1>
         <p>{props.text}</p>
         <a href="/">Take me back</a>
